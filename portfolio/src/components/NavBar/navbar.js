41ad104464e76026c41a48d33b293b310fc2afad
Cameron King <cameronek@mun.ca>
2024-07-04 16:54:20 -0230

    Built the navbar design, still need to implement linking.

--- portfolio/src/components/NavBar/navbar.js
+++ portfolio/src/components/NavBar/navbar.js
@@ -1,9 +1,36 @@
-import React from 'react'
-import './navbar.css'
+import React from 'react';
+import './navbar.css';
+import logo from '../../assets/logoSmall.png';
+import linkedIn from '../../assets/linkedIn.png';
+import github from '../../assets/github.png';
+import email from '../../assets/email.png';
+import { Link } from 'react-scroll';
 
 const navbar = () => {
 	return (
-		<div>navbar</div>
+		<nav className="navbar">
+			<img src={logo} alt="logo" className="logo"/>
+			<div className="desktopMenu">
+				<Link className="desktopMenuListItem">Home </Link>
+				<Link className="desktopMenuListItem">About Me </Link>
+				<Link className="desktopMenuListItem">Projects </Link>
+				<Link className="desktopMenuListItem">Resume </Link>
+			</div>
+
+			<div className="desktopButtonContainer">
+				<button className="desktopMenuButton">
+					<img src={linkedIn} alt="linkedInDesktopButton" className="linkedInDesktopButton"/>
+				</button>
+
+				<button className="desktopMenuButton">
+					<img src={github} alt="githubDesktopButton" className="githubDesktopButton"/>
+				</button>
+
+				<button className="desktopMenuButton">
+					<img src={email} alt="emailDesktopButton" className="emailDesktopButton"/>
+				</button>
+			</div>
+		</nav>
 	)
 }
 
