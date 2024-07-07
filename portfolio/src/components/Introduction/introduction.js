9e5d3ca44ff0a6587eaa2b9c0b7e0275af5b8fa7
Cameron King <cameronek@mun.ca>
2024-07-06 23:17:24 -0230

    Finished styling about me section

--- portfolio/src/components/Introduction/introduction.js
+++ portfolio/src/components/Introduction/introduction.js
@@ -7,36 +7,47 @@ const introduction = () =>
 	return (
 		<section className="introduction">
 
-			<div className = "name">
-				<span>Cameron King</span>
+      <div className = "titles">
 				<br />
 				<br />
+
+	    	<span style={{color:"#8BE9FD"}}>Engineer.  &nbsp;</span>
+				<span style={{color:"#50FA7B"}}>Game Dev. &nbsp;</span>
+				<span style={{color:"#FF79C6"}}>Sports Fan. &nbsp;</span>
+				<span style={{color:"#F1FA8C"}}>Newfoundlander. &nbsp;</span>
 				<br />
+				<br />
+
 			</div>
 
 		  <div className="headshotContainer">
 				<img src={headshot} alt="Profile" className="headshot"/>
       </div>
 
-      <div className = "titles">
+      <div className = "pageBreak">
 				<br />
 				<br />
-
-	    	<span>Software Engineer. &nbsp;  &nbsp; </span>
-				<span>Game Developer. &nbsp;  &nbsp;</span>
-				<span>Sports Fan. &nbsp;  &nbsp;</span>
-				<span>Newfoundlander. &nbsp;  &nbsp;</span>
+      </div>
+			<div className = "name">
+				<span>Hey! I'm Cameron.</span>
 				<br />
 				<br />
-				<br />
-			
 			</div>
+
 			
 			<div className = "description">
-				<span> Hey! I'm a Computer Engineer from Memorial University of Newfoundland's Class of 2024.
-				I have a passion for software development and am always trying to learn an improve myself as an engineer.
-				Here are a few of the things that I've worked on recently.
-				 </span>
+				<br />
+				<br />
+				<span> I am a Computer Engineer from the Memorial University of Newfoundland and 
+					I have a passion for software development. I am always trying to learn and improve myself as an engineer, 
+					by researching and creating new projects. 
+				</span>
+				<br />
+				<br />
+				
+				<span> 
+					Here are a few of the things that I've worked on recently.
+				</span>
 			</div>	
 
 		</section>
