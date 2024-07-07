e7a0070b32b1036971795b28734198bbbb312b7b
Cameron King <cameronek@mun.ca>
2024-07-07 13:42:26 -0230

    added lighthouse silhouette

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,11 @@
 // Main app 
 
 import NavBar from "./components/NavBar/navbar";
+import Lighthouse from "./components/Lighthouse/lighthouse";
 import Introduction from "./components/Introduction/introduction";
+
+// need to create and import A gradient that fades from lighthous black to night blue
+
 // "Home" component will be titled as lighthouse.js
 
 
@@ -9,6 +13,7 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Lighthouse/>
       <Introduction/>
     </div>
   );
---
+++ portfolio/src/components/Lighthouse/lighthouse.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './lighthouse.css';
+import lighthouseImage from '../../assets/lighthouse2.png';
+import lighthouseGround from '../../assets/lighthouseGround.png';
+
+// homepage consists of a lighthouse with stars in the background.
+const lighthouse = () => 
+{
+	return (
+
+		<section className="homepage">
+
+			<div className ="lighthouseSil">
+				<img src={lighthouseImage} className = "lighthouseImage" />
+				<img src={lighthouseGround} className = "groundImage" />	
+			</div>
+
+		</section>
+
+		)
+
+} 
+export default lighthouse;
